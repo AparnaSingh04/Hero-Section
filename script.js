d9aea44ddfffe2f7ab8ef630bf0e07e8f331add7
AparnaSingh04 <singhaparna0404@gmail.com>
2025-03-02 17:24:05 +0530

    script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     text.addEventListener("mouseenter", () => {
-        hoverActive = true;
+        hoverActive = false;
         applyHoverEffects();
     });
 
